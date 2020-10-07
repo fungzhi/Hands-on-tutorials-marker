71c281f2448e627b97363c79fc799c5a79c10446
Tsuikayan <190064032@stu.vtc.edu.hk>
2020-10-07 18:39:11 +0800

    Create an Audio Transcript finish

--- cloudprojectmarker/my-project/app.py
+++ cloudprojectmarker/my-project/app.py
@@ -3,9 +3,13 @@
 from aws_cdk import core
 
 from my_project.my_project_stack import MyProjectStack
+from marking.create_an_audio_transcript import S3Template
+from marking.create_an_audio_transcript import Transcriptfile
+
 
 
 app = core.App()
 MyProjectStack(app, "my-project")
+S3Template, Transcriptfile(app, "marking")
 
 app.synth()
