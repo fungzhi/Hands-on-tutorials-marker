a114623ef602f6f836b1a9dd811d5a5f189e8cc9
JoanPuncy <joanpuncy@gmail.com>
2020-12-24 16:01:55 +0000

    1st Tentative Draft Version

--- cloudprojectmarker/my-project/app.py
+++ cloudprojectmarker/my-project/app.py
@@ -7,7 +7,7 @@ from marking.send_messages_between_distributed_applications import CreateQueue, 
 from marking.create_and_query_a_nosql_table import CreateTable, InputData, QueryData
 from marking.build_a_static_website_with_amazon_s3 import StaticWebsiteBucket
 # Tentative Draft Version
-from marking.creating_an_amazon_cloudFront_distribution import CloudFrontWebSite
+from marking.creating_an_amazon_cloudfront_distribution import CloudFrontWebSite
 
 app = core.App()
 MyProjectStack(app, "my-project")
