ba7258fc1b3a50f44eafae9a7ea68153f5235838
JoanPuncy <joanpuncy@gmail.com>
2020-12-24 15:35:29 +0000

    1st Tentative Draft Version

--- cloudprojectmarker/my-project/app.py
+++ cloudprojectmarker/my-project/app.py
@@ -6,8 +6,10 @@ from my_project.my_project_stack import MyProjectStack
 from marking.send_messages_between_distributed_applications import CreateQueue, SendMessages
 from marking.create_and_query_a_nosql_table import CreateTable, InputData, QueryData
 from marking.build_a_static_website_with_amazon_s3 import StaticWebsiteBucket
+# Tentative Draft Version
+from marking.creating_an_amazon_cloudFront_distribution import CloudFrontWebSite
 
 app = core.App()
 MyProjectStack(app, "my-project")
-CreateQueue, SendMessages, CreateTable, InputData, QueryData, StaticWebsiteBucket(app, "marking")
+CreateQueue, SendMessages, CreateTable, InputData, QueryData, StaticWebsiteBucket, CloudFrontWebSite(app, "marking")
 app.synth()
