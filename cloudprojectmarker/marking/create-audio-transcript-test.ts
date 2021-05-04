ed4cd1b0f56c836e5be9b35c62fba40e29606b17
EC2 Default User <ec2-user@ip-172-31-6-109.ec2.internal>
2021-05-04 10:54:21 +0000

    create-audio-transcript-test.ts

--- cloudprojectmarker/marking/create-audio-transcript-test.ts
+++ cloudprojectmarker/marking/create-audio-transcript-test.ts
@@ -1,5 +1,5 @@
 //Hands-on Lab: Ceate Audio Transcript
-//Tutorial Link: https://aws.amazon.com/getting-started/hands-on/create-audio-transcript-transcribe/?nc1=h_ls//
+//Tutorial Guide: https://aws.amazon.com/getting-started/hands-on/create-audio-transcript-transcribe/?nc1=h_ls//
 
 import { expect } from "chai";
 // if you used the '@types/mocha' method to install mocha type definitions, uncomment the following line
@@ -10,7 +10,11 @@ import { TranscribeService } from "aws-sdk";
 describe("Ceate Audio Transcript", () => {
   const TranscribeService: AWS.TranscribeService = new AWS.TranscribeService();
  
- 
+     // Part 1: Download transcribe-sample.mp3 from https://aws.amazon.com/getting-started/hands-on/create-audio-transcript-transcribe/?nc1=h_ls //
+     // Part 2: Create a S3 Bucket and upload transcribe-sample.mp3 as a object //
+     // Part 3: Go to Amazon Transcribe Service //
+     // Part 4: Create a transcribe job and choose transcribe-sample.mp3, please follow the settings of tutoial guide //
+     // Part 5: Check if the mp3 file has transcribed to text //
      it("should create a transcription job.", async () => {
         const expected = {
             'name': 'sample-transcription-job',
@@ -23,6 +27,6 @@ describe("Ceate Audio Transcript", () => {
         expect(2, "transcription job, in the Input file location on S3 field, paste the link to the transcribe-sample.mp3 file in your S3 bucket."
         ).to.eq(expected);  
         
-    });
+    }); // 1 mark //
 
 });
