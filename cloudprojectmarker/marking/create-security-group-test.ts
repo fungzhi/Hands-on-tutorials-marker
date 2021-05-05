42e2d2fbb60981d975042480e1e8301318da9d27
EC2 Default User <ec2-user@ip-172-31-11-253.ec2.internal>
2021-05-05 09:03:37 +0000

    create-security-group-test.ts

---
+++ cloudprojectmarker/marking/create-security-group-test.ts
@@ -0,0 +1,61 @@
+import { expect } from "chai";
+// if you used the '@types/mocha' method to install mocha type definitions, uncomment the following line
+import "mocha";
+import { Common } from "./common";
+import { EC2 } from "aws-sdk";
+
+
+
+describe("Security Group", () => {
+    let mySg: EC2.SecurityGroup;
+    const common = new Common();
+    before(async () => {
+    mySg = await common.getSgByName("EC2 Security Group");
+    });
+    
+    
+    
+    it("for mySg should set properly(ingress rule 1). ", async () => {
+        expect(1, "mySg with 1 ingress rule").to.equal(
+            mySg.IpPermissions!.length
+        );
+        
+        expect("0.0.0.0/0", "mySg with ingress rule from anywhere.").to.equal(
+            mySg.IpPermissions![0].IpRanges![0].CidrIp
+        );
+
+        expect(80, "mySg with ingress rule for port 80.").to.equal(
+            mySg.IpPermissions![0].ToPort
+        );
+    });
+        
+        
+    it("for mySg should set properly(ingress rule 2). ", async () => {
+        expect(1, "mySg with 1 ingress rule").to.equal(
+            mySg.IpPermissions!.length
+        );
+        expect("0.0.0.0/0", "mySg with ingress rule from anywhere.").to.equal(
+            mySg.IpPermissions![0].IpRanges![0].CidrIp
+        );
+        
+        expect(22, "mySg with ingress rule for port 22.").to.equal(
+            mySg.IpPermissions![0].ToPort
+        );
+    }); 
+
+
+    it("for mySg should set properly(Egress rule). ", async () => {		
+        expect(1, "mySg with only 1 Egress rule").to.equal(
+            mySg.IpPermissionsEgress!.length
+        );
+        
+        expect("0.0.0.0/0", "mySg with Egress rule from anywhere.").to.equal(
+            mySg.IpPermissions![0].IpRanges![0].CidrIp
+        );
+        
+        expect(0, "mySg with Egress rule for All traffic.").to.equal(
+            mySg.IpPermissions![0].ToPort
+        );
+
+    });
+});
